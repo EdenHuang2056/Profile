9e59f193bdfbb7fb5edf5d7f5d4aed3c060493f0
EdenHuang2056 <73862617+EdenHuang2056@users.noreply.github.com>
2021-03-08 10:45:49 +0800

    Update request4.js

--- W2/request4.js
+++ W2/request4.js
@@ -1,11 +1,11 @@
-function twoSum(nums, target){
-    for(i=0; i<nums.lenght; i++){
-        for(j=i+1; j<nums.length; j++){
-            if(nums[i] + nums[j] == target){
-                return('['+i+', '+j+']')
+ function twoSum(nums, target) {
+            for (var i = 0; i < nums.length; i++) {
+                for (var j = 0; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return ('[' + i + ',' + j + ']')
+                    };
+                };
             }
         }
-    }
-}
-result=twoSum([2, 11, 7, 15], 9)
-console.log(result)
+        result = twoSum([2, 11, 7, 15], 9)
+        console.log(result)
